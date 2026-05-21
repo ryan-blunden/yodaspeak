5878f87bd2aef8ccba8b030beb7dbf0438986669
Ryan Blunden <ryan.blunden@gmail.com>
2026-05-21 16:29:11 +1000

    chore: simplify env defaults

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -1,11 +1,10 @@
-import multiprocessing
 import os
 
 bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
 accesslog = "-"
 access_log_format = "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s' in %(D)sµs"  # noqa: E501
 
-workers = int(os.getenv("WEB_CONCURRENCY", multiprocessing.cpu_count() * 2))
+workers = int(os.getenv("WEB_CONCURRENCY", 2))
 threads = int(os.getenv("PYTHON_MAX_THREADS", 1))
 
 timeout = int(os.getenv("WEB_TIMEOUT", 120))
